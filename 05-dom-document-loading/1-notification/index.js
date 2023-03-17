f34fb8dfe1477ebc4733cbd6fc4592719712edb3
ME-LocalBody <localbody@yandex.ru>
2023-03-17 17:09:55 +0300

    tasks 05

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,72 @@
 export default class NotificationMessage {
+  static activeNotification = null
+  timerID = null
 
+  constructor(message = '', { duration = 2000, type = 'success' } = {}) {
+    this.type = type
+    this.duration = duration
+    this.message = message
+
+    this.render()
+    this.initEventListeners()
+  }
+
+  getTemplate() {
+    return `
+    <div class="notification ${this.type}" style="--value:${
+      this.duration / 1000
+    }s">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">Notification</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+    </div>
+      `
+  }
+
+  render() {
+    const element = document.createElement('div') // (*)
+
+    element.innerHTML = this.getTemplate()
+
+    // NOTE: в этой строке мы избавляемся от обертки-пустышки в виде `div`
+    // который мы создали на строке (*)
+    this.element = element.firstElementChild
+  }
+
+  show(parent = document.body) {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove()
+    }
+
+    parent.append(this.element)
+
+    this.timerID = setTimeout(() => {
+      this.remove()
+    }, this.duration)
+
+    NotificationMessage.activeNotification = this
+  }
+
+  initEventListeners() {
+    // NOTE: в данном методе добавляем обработчики событий, если они есть
+  }
+
+  remove() {
+    clearTimeout(this.timerID)
+
+    if (this.element) {
+      this.element.remove()
+    }
+  }
+
+  destroy() {
+    this.remove()
+    this.element = null
+    NotificationMessage.activeNotification = null
+    // NOTE: удаляем обработчики событий, если они есть
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,57 @@
 export default class SortableTable {
   constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig
+    this.data = data
 
+    this.render()
+    this.initEventListeners()
+  }
+
+  getTemplate() {
+    return `
+      <div data-element="header" class="sortable-table__header sortable-table__row">
+        <div class="sortable-table__cell" data-id="images" data-sortable="false" data-order="asc">
+          <span>Image</span>
+        </div>
+        <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="asc">
+          <span>Name</span>
+          <span data-element="arrow" class="sortable-table__sort-arrow">
+            <span class="sort-arrow"></span>
+          </span>
+        </div>
+        <div class="sortable-table__cell" data-id="quantity" data-sortable="true" data-order="asc">
+          <span>Quantity</span>
+        </div>
+        <div class="sortable-table__cell" data-id="price" data-sortable="true" data-order="asc">
+          <span>Price</span>
+        </div>
+        <div class="sortable-table__cell" data-id="sales" data-sortable="true" data-order="asc">
+          <span>Sales</span>
+        </div>
+      </div>
+    `
+  }
+
+  render() {
+    const element = document.createElement('div') // (*)
+
+    element.innerHTML = this.getTemplate()
+
+    // NOTE: в этой строке мы избавляемся от обертки-пустышки в виде `div`
+    // который мы создали на строке (*)
+    this.element = element.firstElementChild
+  }
+
+  initEventListeners() {
+    // NOTE: в данном методе добавляем обработчики событий, если они есть
+  }
+
+  remove() {
+    this.element.remove()
+  }
+
+  destroy() {
+    this.remove()
+    // NOTE: удаляем обработчики событий, если они есть
   }
 }
-
